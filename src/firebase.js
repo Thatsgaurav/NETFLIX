9978838ce0d799c56dc964b4eb0323f28bd275f8
Gaurav Singh <0201csai117@niet.co.in>
2021-04-14 17:18:01 +0100

    commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8fCi5cDQA6i5NiUmLurxRw0i-aE6prPQ",
+    authDomain: "netflix-c45a9.firebaseapp.com",
+    projectId: "netflix-c45a9",
+    storageBucket: "netflix-c45a9.appspot.com",
+    messagingSenderId: "205989615041",
+    appId: "1:205989615041:web:07c0cb815352c501028b40"
+  };
